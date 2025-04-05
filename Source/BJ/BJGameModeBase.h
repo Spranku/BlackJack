148ffe38848c976fbedd384b74295858a2b76717
Spranky <fvs@imt.zone>
2025-04-06 02:15:27 +0300

    Setup

---
+++ Source/BJ/BJGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BJGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BJ_API ABJGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
