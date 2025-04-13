804cb81e681a99f6a4506bef79d40306a82ef041
Spranky <fvs@imt.zone>
2025-04-14 01:33:22 +0300

    First Build

--- Source/BJ/BJCharacter.cpp
+++ Source/BJ/BJCharacter.cpp
@@ -18,7 +18,7 @@ ABJCharacter::ABJCharacter()
 	FirstPersonCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
 	FirstPersonCameraComponent->SetupAttachment(GetCapsuleComponent()); 
 	FirstPersonCameraComponent->bUsePawnControlRotation = true;
-	FirstPersonCameraComponent->SetFieldOfView(90.0f);
+	FirstPersonCameraComponent->SetFieldOfView(110.0f);
 }
 
 // Called when the game starts or when spawned
